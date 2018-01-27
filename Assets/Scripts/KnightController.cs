904bd2a73eada2a09af8ca7ba9571515186a8db5
Poulad <poulad.pld@gmail.com>
2018-01-27 14:21:11 -0500

    Initial commit

---
+++ Assets/Scripts/KnightController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class KnightController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    public float MaxSpeed;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var move = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector2(move * MaxSpeed, _rb.velocity.y);
+
+        var animator = GetComponent<Animator>();
+        
+    }
+}
